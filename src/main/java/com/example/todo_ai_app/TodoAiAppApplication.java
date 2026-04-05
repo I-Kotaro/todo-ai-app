e482f872111b97c0d425cf9d5bffd354647444d3
Kotaro <ino.kota0602@gmail.com>
2026-04-05 10:28:47 +0900

    プロジェクト新規作成

---
+++ src/main/java/com/example/todo_ai_app/TodoAiAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.todo_ai_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/todo_ai_app/TodoAiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todo_ai_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
